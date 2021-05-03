a6120762d5b74dbd73c0d8b754ee07253bdccb23
Bangmond <dembbuto@naver.com>
2021-05-04 01:43:05 +0900

    Create-React-App & React Router Dom

---
+++ client/src/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+import LandingPage from './components/views/LandingPage/LandginPage'
+import LoginPage from './components/views/LoginPage/LoginPage'
+import RegisterPage from './components/views/RegisterPage/RegisterPage'
+
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/register" component={RegisterPage} />
+        </Switch>
+      </div>
+    </Router>
+  );
+}
---
+++ client/src/components/views/LandingPage/LandginPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LandginPage() {
+  return (
+    <div>
+      LandginPage
+    </div>
+  )
+}
+
+export default LandginPage
