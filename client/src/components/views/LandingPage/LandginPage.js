521ee9d215da042681efba8ba5ff179d99b15989
Bangmond <dembbuto@naver.com>
2021-05-07 14:12:44 +0900

    로그인 페이지

--- client/src/components/views/LandingPage/LandginPage.js
+++ client/src/components/views/LandingPage/LandginPage.js
@@ -11,8 +11,10 @@ function LandginPage() {
   }, [])
 
   return (
-    <div>
-      LandginPage
+    <div style={{
+      display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
+    }}>
+      <h2>시작 페이지</h2>
     </div>
   )
 }
